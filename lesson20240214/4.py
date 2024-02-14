ecf9a644706b1df116c457b9bc3deb078a2e5189
Коля <kolja.shevtsov.00@gmail.com>
2024-02-14 22:12:52 +0200

    1

---
+++ lesson20240214/4.py
@@ -0,0 +1,16 @@
+from turtle import *
+a= int(input('Яка буде сторона квадрату?'))
+b= a-20
+color('blue')
+width(3)
+for i in range(4):
+    fd(a)
+    right(90)
+penup()
+fd(a+20)
+pendown()
+color('red')
+for i in range(4):
+    fd(b)
+    right(90)
+exitonclick()
