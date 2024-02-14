ae39377f1c8af39836071e857de35f58f78643e5
Коля <kolja.shevtsov.00@gmail.com>
2024-02-14 22:11:23 +0200

    1

---
+++ lesson20240214/3.py
@@ -0,0 +1,10 @@
+Vboat = int(input('Яка буде швидкість човна в км/год?'))
+Vtechiya = int(input('Яка буде швидкість течії км/год?'))
+t1 = 2
+t2 = 3
+
+
+Slake = t1 * Vboat
+Sriver = t2 * (Vboat + Vtechiya)
+S = Slake + Sriver
+print("Всього човен проплив - ", S, "км")
